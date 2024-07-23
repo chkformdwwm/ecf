abff82359cbfa7be4e4035382a38c648fed2ed29
chkformdwwm <formationdwwmgraduate@gmail.com>
2024-07-23 08:28:38 +0200

    Add files via upload

---
+++ assets/templates/footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="./../../index.php" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+            <li class="nav-item"><a href="./../../habitats.php" class="nav-link px-2 text-body-secondary">Habitats</a></li>
+            <li class="nav-item"><a href="./../../services.php" class="nav-link px-2 text-body-secondary">Services</a></li>
+            <li class="nav-item"><a href="./../../avis.php" class="nav-link px-2 text-body-secondary">Avis</a></li>
+            <li class="nav-item"><a href="./../../contact.php" class="nav-link px-2 text-body-secondary">Contact</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+    </footer>
+
+</div>
---
+++ assets/templates/navbar.php
@@ -0,0 +1,36 @@
+         <nav class="navbar navbar-expand-md fixed-top d-flex justify-content-center pt-5 mt-1">
+             <div class="d-flex flex-wrap py-3 mb-4 position-absolute custom_nav custom_nav--scrolled container-fluid">
+                 <div class="col-md-3 mb-2 ms-5 mb-md-0 logo">
+                     <a class=" link-body-emphasis text-decoration-none" href="/">
+                         <img src="../../assets/img/logo.png" class="w-75 navbar-brand fixed-width"> </a>
+                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbar-Supported-Content" aria-expanded="false" aria-label="Toggle navigation">
+                         <span class="navbar-toggler-icon"></span>
+                     </button>
+                 </div>
+
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 me-5">
+                         <li><a href="index.php" class="nav-element px-2">Accueil</a></li>
+                         <li class="nav-item dropdown"><a class="nav-element dropdown-toggle" href="habitats.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">Habitats</a>
+                             <ul class="dropdown-menu">
+                                 <li><a class="dropdown-item" href="habitats.php">Tous les habitats</a></li>
+                                 <li><a class="dropdown-item" href="savane.php">Savane</a></li>
+                                 <li><a class="dropdown-item" href="jungle.php">Jungle</a></li>
+                                 <li><a class="dropdown-item" href="marais.php">Marais</a></li>
+                             </ul>
+                         <li class="nav-item dropdown"><a class="nav-element dropdown-toggle" href="habitats.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
+                             <ul class="dropdown-menu">
+                                 <li><a class="dropdown-item" href="services.php">Tous les services</a></li>
+                                 <li><a class="dropdown-item" href="restauration.php">Restauration</a></li>
+                                 <li><a class="dropdown-item" href="animations.php">Animations</a></li>
+
+                             </ul>
+                         <li><a href="avis.php" class="nav-element px-2">Avis</a></li>
+                         <li><a href="contact.php" class="nav-element px-2">Contact</a></li>
+                     </ul>
+
+                     <div class="col-md-3 text-end">
+                         <a href="login.php"> <button type="button" class="btn btn-outline-warning me-2">Connexion</button></a>
+                     </div>
+                 </div>
+         </nav>
